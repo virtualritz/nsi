9fb7789ef14ff5b405289ed73eb8cbbefb2b62bb
Moritz Moeller <virtualritz@gmail.com>
2020-03-11 04:13:39 +0100

    Initial commit.

---
+++ src/argument.rs
@@ -0,0 +1,232 @@
+use std::ffi::CString;
+
+//#[derive(Debug)]
+struct Arg<'a> {
+    name: CString,
+    data: &'a dyn ToNSI,
+    type_of: Type,
+    array_length: usize, // length of each element
+    count: usize,        // number of elements
+    flags: u32,
+}
+
+type ArgVec<'a> = Vec<Arg<'a>>;
+
+fn get_c_param_vec<'a>(
+    args_in: &'a ArgVec,
+    args_out: &'a mut Vec<nsi_sys::NSIParam_t>,
+) {
+    *args_out =
+        args_in.iter().map(|arg| arg.to_nsi()).collect::<Vec<_>>();
+}
+
+impl<'a> Arg<'a> {
+    pub fn new(name: &str, data: &'a dyn ToNSI) -> Self {
+        Arg {
+            name: CString::new(name).unwrap(),
+            data,
+            type_of: data.type_nsi(),
+            array_length: 1,
+            count: data.len_nsi(),
+            flags: 0,
+        }
+    }
+
+    pub fn to_nsi(&'a self) -> nsi_sys::NSIParam_t {
+        assert!(
+            self.data.len_nsi() % (self.array_length * self.count) == 0
+        );
+        nsi_sys::NSIParam_t {
+            name: self.name.as_ptr(),
+            data: self.data.as_ptr_nsi(),
+            type_: self.type_of as i32,
+            arraylength: self.array_length as i32,
+            count: self.count,
+            flags: self.flags as std::os::raw::c_int,
+        }
+    }
+
+    fn type_of(mut self, type_of: Type) -> Self {
+        // FIXME: check if we fit in data.count() without remainder
+        self.type_of = type_of;
+        self
+    }
+
+    fn count(mut self, count: usize) -> Self {
+        assert!(count * self.array_length <= self.data.len_nsi());
+        assert!(self.data.len_nsi() % count == 0);
+
+        self.count = count;
+        self.array_length = self.data.len_nsi() / count;
+        self
+    }
+
+    fn array_length(mut self, array_length: usize) -> Self {
+        assert!(self.count * array_length <= self.data.len_nsi());
+        assert!(self.data.len_nsi() % array_length == 0);
+
+        self.array_length = array_length;
+        self.count = self.data.len_nsi() / array_length;
+        self
+    }
+
+    fn flags(mut self, flags: u32) -> Self {
+        self.flags = flags;
+        self
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum Type {
+    Invalid = -1,      //nsi_sys::NSIType_t::NSITypeInvalid,
+    Float = 0,         // nsi_sys::NSIType_t::NSITypeFloat,
+    Double = 1 | 0x10, // nsi_sys::NSIType_t::NSITypeFloat | 0x10,
+    Integer = 2,       // nsi_sys::NSIType_t::NSITypeInteger,
+    String = 3,        // nsi_sys::NSIType_t::NSITypeString,
+    Color = 4,         // nsi_sys::NSIType_t::NSITypeColor,
+    Point = 5,         // nsi_sys::NSIType_t::NSITypePoint,
+    Vector = 6,        // nsi_sys::NSIType_t::NSITypeVector,
+    Normal = 7,        // nsi_sys::NSIType_t::NSITypeNormal,
+    Matrix = 8,        // nsi_sys::NSIType_t::NSITypeMatrix,
+    DoubleMatrix = 8 | 0x10, /* nsi_sys::NSIType_t::NSITypeMatrix |
+                        * 0x10, */
+    Pointer = 10, // nsi_sys::NSIType_t::NSITypePointer,
+}
+
+pub trait ToNSI {
+    fn as_ptr_nsi(&self) -> *const ::std::os::raw::c_void;
+    fn len_nsi(&self) -> usize;
+    fn type_nsi(&self) -> Type;
+}
+
+impl<T> ToNSI for T {
+    default fn as_ptr_nsi(&self) -> *const ::std::os::raw::c_void {
+        self as *const _ as _
+    }
+    default fn len_nsi(&self) -> usize {
+        1
+    }
+    default fn type_nsi(&self) -> Type {
+        Type::Invalid
+    }
+}
+
+impl ToNSI for f32 {
+    default fn type_nsi(&self) -> Type {
+        Type::Float
+    }
+}
+
+impl ToNSI for f64 {
+    default fn type_nsi(&self) -> Type {
+        Type::Double
+    }
+}
+
+impl ToNSI for i32 {
+    default fn type_nsi(&self) -> Type {
+        Type::Integer
+    }
+}
+
+impl ToNSI for String {
+    default fn type_nsi(&self) -> Type {
+        Type::String
+    }
+}
+
+impl ToNSI for [f32; 3] {
+    default fn type_nsi(&self) -> Type {
+        Type::Color
+    }
+}
+
+impl ToNSI for [f32; 16] {
+    default fn type_nsi(&self) -> Type {
+        Type::Matrix
+    }
+}
+
+impl ToNSI for [f64; 16] {
+    default fn type_nsi(&self) -> Type {
+        Type::DoubleMatrix
+    }
+}
+
+impl<T> ToNSI for *const T {
+    default fn type_nsi(&self) -> Type {
+        Type::Pointer
+    }
+}
+
+impl<T> ToNSI for Vec<T> {
+    fn as_ptr_nsi(&self) -> *const ::std::os::raw::c_void {
+        self.as_ptr() as _
+    }
+    fn len_nsi(&self) -> usize {
+        self.len()
+    }
+    default fn type_nsi(&self) -> Type {
+        Type::Invalid
+    }
+}
+
+impl ToNSI for Vec<f32> {
+    default fn type_nsi(&self) -> Type {
+        Type::Float
+    }
+}
+
+impl ToNSI for Vec<f64> {
+    default fn type_nsi(&self) -> Type {
+        Type::Double
+    }
+}
+
+impl ToNSI for Vec<i32> {
+    default fn type_nsi(&self) -> Type {
+        Type::Integer
+    }
+}
+
+impl ToNSI for Vec<String> {
+    default fn type_nsi(&self) -> Type {
+        Type::String
+    }
+}
+
+impl ToNSI for Vec<[f32; 3]> {
+    default fn type_nsi(&self) -> Type {
+        Type::Color
+    }
+}
+
+impl ToNSI for Vec<[f32; 16]> {
+    default fn type_nsi(&self) -> Type {
+        Type::Matrix
+    }
+}
+
+impl ToNSI for Vec<[f64; 16]> {
+    default fn type_nsi(&self) -> Type {
+        Type::DoubleMatrix
+    }
+}
+
+impl<T> ToNSI for Vec<*const T> {
+    default fn type_nsi(&self) -> Type {
+        Type::Pointer
+    }
+}
+
+#[test]
+fn test() {
+    let single = Arg::new("foo", &10.0f32);
+    let vector = vec![1.0f32, 2.0f32, 3.0f32, 4.0f32];
+    let array = Arg::new("bar", &vector).count(2); // 2x2 array of f32
+
+    let mut result_vec = Vec::<nsi_sys::NSIParam_t>::new();
+    get_c_param_vec(&vec![single, array], &mut result_vec);
+
+    dbg!(result_vec);
+}
---
+++ src/lib.rs
@@ -0,0 +1,344 @@
+#![feature(specialization)]
+//#![feature(const_generics)]
+#![feature(type_alias_impl_trait)]
+
+//#![allow(non_upper_case_globals)]
+//#![allow(non_camel_case_types)]
+//#![allow(non_snake_case)]
+
+//#![feature(associated_type_bounds)]
+
+static STR_ERROR: &str = "Found null byte in the middle of the string";
+
+extern crate nsi_sys;
+
+include!("argument.rs");
+
+use std::{ops::Drop, vec::Vec};
+
+//type Handle = impl Into<Vec<u8>>;
+
+struct Context {
+    context: nsi_sys::NSIContext_t,
+}
+
+impl Context {
+    pub fn new(args: &ArgVec) -> Self {
+        Self {
+            context: {
+                if args.is_empty() {
+                    unsafe { nsi_sys::NSIBegin(0, std::ptr::null()) }
+                } else {
+                    let mut args_out =
+                        Vec::<nsi_sys::NSIParam_t>::new();
+                    get_c_param_vec(args, &mut args_out);
+
+                    unsafe {
+                        nsi_sys::NSIBegin(
+                            args_out.len() as i32,
+                            args_out.as_ptr()
+                                as *const nsi_sys::NSIParam_t,
+                        )
+                    }
+                }
+            },
+        }
+    }
+
+    pub fn create(
+        &mut self,
+        handle: impl Into<Vec<u8>>,
+        node_type: &Node,
+        args: &ArgVec,
+    ) {
+        let mut args_out = Vec::<nsi_sys::NSIParam_t>::new();
+        get_c_param_vec(args, &mut args_out);
+
+        unsafe {
+            nsi_sys::NSICreate(
+                self.context,
+                CString::new(handle.into()).expect(STR_ERROR).as_ptr(),
+                node_type.as_c_str().as_ptr() as *const i8,
+                args_out.len() as i32,
+                args_out.as_ptr() as *const nsi_sys::NSIParam_t,
+            )
+        }
+    }
+
+    pub fn delete(
+        &mut self,
+        handle: impl Into<Vec<u8>>,
+        args: &ArgVec,
+    ) {
+        let mut args_out = Vec::<nsi_sys::NSIParam_t>::new();
+        get_c_param_vec(args, &mut args_out);
+
+        unsafe {
+            nsi_sys::NSIDelete(
+                self.context,
+                CString::new(handle.into()).expect(STR_ERROR).as_ptr(),
+                args_out.len() as i32,
+                args_out.as_ptr() as *const nsi_sys::NSIParam_t,
+            );
+        }
+    }
+
+    fn set_attribute(
+        &mut self,
+        object: impl Into<Vec<u8>>,
+        args: &ArgVec,
+    ) {
+        let mut args_out = Vec::<nsi_sys::NSIParam_t>::new();
+        get_c_param_vec(args, &mut args_out);
+
+        unsafe {
+            nsi_sys::NSISetAttribute(
+                self.context,
+                CString::new(object.into()).expect(STR_ERROR).as_ptr(),
+                args_out.len() as i32,
+                args_out.as_ptr() as *const nsi_sys::NSIParam_t,
+            );
+        }
+    }
+
+    fn set_attribute_at_time(
+        &mut self,
+        object: impl Into<Vec<u8>>,
+        time: f64,
+        args: &ArgVec,
+    ) {
+        let mut args_out = Vec::<nsi_sys::NSIParam_t>::new();
+        get_c_param_vec(args, &mut args_out);
+
+        unsafe {
+            nsi_sys::NSISetAttributeAtTime(
+                self.context,
+                CString::new(object.into()).expect(STR_ERROR).as_ptr(),
+                time,
+                args_out.len() as i32,
+                args_out.as_ptr() as *const nsi_sys::NSIParam_t,
+            );
+        }
+    }
+
+    fn connect(
+        &mut self,
+        from: impl Into<Vec<u8>>,
+        from_attr: impl Into<Vec<u8>>,
+        to: impl Into<Vec<u8>>,
+        to_attr: impl Into<Vec<u8>>,
+        args: &ArgVec,
+    ) {
+        let mut args_out = Vec::<nsi_sys::NSIParam_t>::new();
+        get_c_param_vec(args, &mut args_out);
+
+        unsafe {
+            nsi_sys::NSIConnect(
+                self.context,
+                CString::new(from.into()).expect(STR_ERROR).as_ptr(),
+                CString::new(from_attr.into())
+                    .expect(STR_ERROR)
+                    .as_ptr(),
+                CString::new(to.into()).expect(STR_ERROR).as_ptr(),
+                CString::new(to_attr.into()).expect(STR_ERROR).as_ptr(),
+                args_out.len() as i32,
+                args_out.as_ptr() as *const nsi_sys::NSIParam_t,
+            );
+        }
+    }
+
+    fn disconnect(
+        &mut self,
+        from: impl Into<Vec<u8>>,
+        from_attr: impl Into<Vec<u8>>,
+        to: impl Into<Vec<u8>>,
+        to_attr: impl Into<Vec<u8>>,
+    ) {
+        unsafe {
+            nsi_sys::NSIDisconnect(
+                self.context,
+                CString::new(from.into()).expect(STR_ERROR).as_ptr(),
+                CString::new(from_attr.into())
+                    .expect(STR_ERROR)
+                    .as_ptr(),
+                CString::new(to.into()).expect(STR_ERROR).as_ptr(),
+                CString::new(to_attr.into()).expect(STR_ERROR).as_ptr(),
+            );
+        }
+    }
+
+    fn evaluate(&mut self, args: &ArgVec) {
+        let mut args_out = Vec::<nsi_sys::NSIParam_t>::new();
+        get_c_param_vec(args, &mut args_out);
+
+        unsafe {
+            nsi_sys::NSIEvaluate(
+                self.context,
+                args_out.len() as i32,
+                args_out.as_ptr() as *const nsi_sys::NSIParam_t,
+            );
+        }
+    }
+
+    fn render_control(&mut self, args: &ArgVec) {
+        let mut args_out = Vec::<nsi_sys::NSIParam_t>::new();
+        get_c_param_vec(args, &mut args_out);
+
+        unsafe {
+            nsi_sys::NSIRenderControl(
+                self.context,
+                args_out.len() as i32,
+                args_out.as_ptr() as *const nsi_sys::NSIParam_t,
+            );
+        }
+    }
+}
+
+impl Drop for Context {
+    fn drop(&mut self) {
+        unsafe {
+            //(len, arg_list) = nsi_sys_arg_list!( args );
+            nsi_sys::NSIEnd(self.context);
+        }
+    }
+}
+
+pub enum Node {
+    Root,
+    Global,
+    Set,
+    Shader,
+    Attributes,
+    Transform,
+    Instances,
+    Mesh,
+    FaceSet,
+    Curves,
+    Particles,
+    Procedural,
+    Volume,
+    Environment,
+    Camera,
+    /*OrthographicCamera,
+    PerspectiveCamera,
+    FisheyeCamera,
+    CylindricalCamera,
+    SphericalCamera,*/
+    Outputdriver,
+    Outputlayer,
+    Screen,
+}
+
+impl Node {
+    pub fn as_c_str(&self) -> &'static [u8] {
+        match *self {
+            Node::Root => b".root\0",
+            Node::Global => b".global\0",
+            Node::Set => b"set\0",
+            Node::Shader => b"set\0",
+            Node::Attributes => b"attributes\0",
+            Node::Transform => b"transform\0",
+            Node::Instances => b"instances\0",
+            Node::Mesh => b"mesh\0",
+            Node::FaceSet => b"faceset\0",
+            Node::Curves => b"curves\0",
+            Node::Particles => b"particles\0",
+            Node::Procedural => b"procedural\0",
+            Node::Volume => b"volume\0",
+            Node::Environment => b"environment\0",
+            Node::Camera => b"camera\0",
+            Node::Outputdriver => b"outputdriver\0",
+            Node::Outputlayer => b"outputlayer\0",
+            Node::Screen => b"screen\0",
+        }
+    }
+}
+
+/*
+#[test]
+test_create() {
+    let nsi_context = nsi::Context::new(no_args!)
+
+    nsi_context.create(
+        "main_camera",
+        "perpectivecamera",
+        build_parameter_list!(
+            ( "depthoffield.enable", true ),
+            ( "depthoffield.fstop", f_stop ),
+            ( "depthoffield.focallength", focal_length ),
+            ( "depthoffield.focaldistance", focal_distance ),
+            ( "depthoffield.aperture.enable", true ),
+            ( "depthoffield.aperture.sides", {
+                    match camera_stream_values[ &ae_sys::AEGP_LayerStream_IRIS_SHAPE ] {
+                        1 => 4 // 1 == fast rectangle == 4 sides
+                        _ =>
+                    }
+                }
+            ),
+            ( "depthoffield.aperture.angle", camera_stream_values[ &ae_sys::AEGP_LayerStream_IRIS_ROTATION ] )
+         )
+    );
+
+}*/
+
+/*fn param( &mut self, Arg) {
+
+}*/
+
+/*
+macro_rules! parameter_list {
+    // The pattern for a single `eval`
+    (( $e:expr, $e:expr)) => {{
+        {
+            let val: usize = $e; // Force types to be integers
+            println!("{} = {}", stringify!{$e}, val);
+        }
+    }};
+
+    // Decompose multiple `eval`s recursively
+    (eval $e:expr, $(eval $es:expr),+) => {{
+        calculate! { eval $e }
+        calculate! { $(eval $es),+ }
+    }};
+}*/
+
+macro_rules! make_list(
+    () => (
+        None
+    );
+    ( $x:expr $( , $more:expr )* ) => (
+        Node::new($x, make_list!( $( $more ),* ))
+    )
+);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+
+    fn it_works() {
+        let nsi_context: Context;
+
+        /*let foo: Arg = {
+            name: ""
+        };*/
+
+        let nsi_context = nsi::Context::new(&ArgVec::new());
+
+        //   .param( "type", "cloud" )
+        //    .param( errorhandler, my_error_handler )
+
+        /*nsi_context
+        .set_attribute("handle")
+        .param("fov", 45.0)
+        .param("depthoffield.fstop", 4.0);*/
+
+        nsi_context
+            .render_control(&ArgVec::new(Arg::new("action", "start")));
+    }
+
+    // nsi::Attribute::new("handle")
+    //    .add( "fov", 45.0f )
+    //    .add( "depthoffield.fstop", 4.0 )
+}
